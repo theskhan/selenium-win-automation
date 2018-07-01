c2766664424c33b11519852363f580abf5383817
Izappworld <izappworld@gmail.com>
2018-07-01 20:32:17 +0530

    Initial commit

---
+++ ZeroAutomation/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.White.UIItems.TableItems;
+
+namespace ZeroAutomation
+{
+    public static class Extensions
+    {
+        public static string NameTrimmed(this TableCell cell)
+        {
+            return cell.Name.Split(new string[] { " Row" }, StringSplitOptions.None)[0];
+        }
+    }
+}
---
+++ ZeroAutomation/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using TestStack.White;
+using TestStack.White.Factory;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.TableItems;
+using System.Threading;
+
+namespace ZeroAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application application = Application.Attach(@"Pi");
+            Window window = application.GetWindow(SearchCriteria.ByAutomationId("frmMain"), InitializeOption.WithCache);
+
+            Panel alertPanel = window.Get<Panel>("ctlScanner");
+
+            Table alertTable = alertPanel.Get<Table>(SearchCriteria.Indexed(0));
+
+            foreach (var row in alertTable.Rows)
+            {
+                Console.WriteLine(row.Name + ": ");
+                foreach(var cell in row.Cells)
+                {
+                    Console.Write(cell.NameTrimmed() + ": " + cell.Value + ", ");
+
+                    if (cell.NameTrimmed() == "Trade")
+                    {
+                        cell.Click();
+                        window.WaitWhileBusy();
+                        window.WaitTill(() => WhenOrderWindowAppears(window));
+
+                        // Wait till order window appears
+                        Console.WriteLine("Order window pened");
+
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        private static bool WhenOrderWindowAppears(Window window)
+        {
+            Window orderWindow = window.Get<Window>(SearchCriteria.Indexed(0));
+            if (orderWindow != null)
+                return true;
+            else
+                return false;
+        }
+    }
+}
